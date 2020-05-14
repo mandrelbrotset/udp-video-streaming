7581c9d16d874f50113bf13008d5de25c6dbc124
Sheriff Olaoye <sheriffolaoye@gmail.com>
2020-05-14 13:09:18 -0500

    renamed files properly

--- udp_client.py
+++ udp_client.py
@@ -1,47 +1,52 @@
 import cv2
 import socket
+import math
 import pickle
-import numpy as np
 
-host = "0.0.0.0"
+max_length = 65000
+host = "192.168.0.2"
 port = 5000
-max_length = 65540
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((host, port))
 
-frame_info = None
-buffer = None
-frame = None
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
 
-print("-> waiting for connection")
+while ret:
+    # compress frame
+    retval, buffer = cv2.imencode(".jpg", frame)
 
-while True:
-    data, address = sock.recvfrom(max_length)
+    if retval:
+        # convert to byte array
+        buffer = buffer.tobytes()
+        # get size of the frame
+        buffer_size = len(buffer)
+
+        num_of_packs = 1
+        if buffer_size > max_length:
+            num_of_packs = math.ceil(buffer_size/max_length)
+
+        frame_info = {"packs":num_of_packs}
+
+        # send the number of packs to be expected
+        print("Number of packs:", num_of_packs)
+        sock.sendto(pickle.dumps(frame_info), (host, port))
+        
+        left = 0
+        right = max_length
+
+        for i in range(num_of_packs):
+            print("left:", left)
+            print("right:", right)
+
+            # truncate data to send
+            data = buffer[left:right]
+            left = right
+            right += max_length
+
+            # send the frames accordingly
+            sock.sendto(data, (host, port))
     
-    if len(data) < 100:
-        frame_info = pickle.loads(data)
+    ret, frame = cap.read()
 
-        if frame_info:
-            nums_of_packs = frame_info["packs"]
-
-            for i in range(nums_of_packs):
-                data, address = sock.recvfrom(max_length)
-
-                if i == 0:
-                    buffer = data
-                else:
-                    buffer += data
-
-            frame = np.frombuffer(buffer, dtype=np.uint8)
-            frame = frame.reshape(frame.shape[0], 1)
-
-            frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-            frame = cv2.flip(frame, 1)
-            
-            if frame is not None and type(frame) == np.ndarray:
-                cv2.imshow("Stream", frame)
-                if cv2.waitKey(1) == 27:
-                    break
-                
-print("goodbye")
+print("done")
